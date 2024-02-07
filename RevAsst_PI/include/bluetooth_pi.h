24da5740ca53f66063415b502a3b6cbaf13c31a6
sohailjaffer <sohailjaffer13@gmail.com>
2024-02-07 16:48:46 +0000

    removed bluetooth stuffgit add .

--- RevAsst_PI/include/bluetooth_pi.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
-#include <unistd.h> 
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
-
-extern int bluetooth_tester();
