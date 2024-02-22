94555f1024469c38cbd942f164fd068624946de3
TWKevinHaung <karta1764514@gmail.com>
2024-02-22 17:58:36 +0000

    Try to setting up the class funtion

--- RevAsst_PI/src/ultrasound_sensor.cpp
+++ RevAsst_PI/src/ultrasound_sensor.cpp
@@ -1,8 +1,25 @@
+
+
+// KEVIN MADE CHANGES HERE
 #include "ultrasound_sensor.h"
+#include "studio.h"
+#include "pigpio.h"
 
 
 void InitUltrasound(){
 
+int timeout = 50000;
 
-// KEVIN MADE CHANGES HERE
+void SetupPinFutions(unit trigPin, unit echoPin)
+{
+    Gpio_initialise(trigPin);
+    Gpio_initialise(echoPin);
+    Gpio_pin_set(trigPin, GPIO_OUT);
+    Gpio_pin_set(ehcoPin, GPIO_IN);
+}
+int getCm(unit trigPin, unit echoPin);
+{
+    unite64_t pulseLength = getfeedback(trigPin, echoPin);
+    return pulselength / 29/ 2;
+}
 }
