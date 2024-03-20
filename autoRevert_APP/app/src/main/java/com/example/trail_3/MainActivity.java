310a81a7b9925a19a452fef38398eeb2c95d9c74
Zeeshan Mistry <157168096+ZeeshanMistry@users.noreply.github.com>
2024-03-20 15:49:12 +0000

    Added Direction button functions

--- autoRevert_APP/app/src/main/java/com/example/trail_3/MainActivity.java
+++ autoRevert_APP/app/src/main/java/com/example/trail_3/MainActivity.java
@@ -8,20 +8,50 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button Left;
+    Button Right;
+    Button Forward;
+    Button Backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button forwards;
-        
-        forwards = this.findViewById(R.id.button);
-        
-        forwards.setOnClickListener(new View.OnClickListener() {
+
+        Left = this.findViewById(R.id.button);
+        Right = this.findViewById(R.id.button3);
+        Forward = this.findViewById(R.id.button2);
+        Backward = this.findViewById(R.id.button4);
+
+        Left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
             }
+
+
+
+
         });
+        Right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Forward", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Backward", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
