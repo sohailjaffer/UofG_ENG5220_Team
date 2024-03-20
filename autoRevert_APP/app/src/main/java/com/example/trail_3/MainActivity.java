e9b246889818fe27f57073496d7ce208a3a3efc4
Zeeshan Mistry <157168096+ZeeshanMistry@users.noreply.github.com>
2024-03-20 15:20:37 +0000

    New Interface

---
+++ autoRevert_APP/app/src/main/java/com/example/trail_3/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.trail_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button forwards;
+        
+        forwards = this.findViewById(R.id.button);
+        
+        forwards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Zeeshan", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
