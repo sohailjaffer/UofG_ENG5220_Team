34fed59210c93017cb48c578f6a640d42c89b02e
Zeeshan Mistry <157168096+ZeeshanMistry@users.noreply.github.com>
2024-03-20 16:58:46 +0000

    Ip Address dialogue box added

--- autoRevert_APP/app/src/main/java/com/example/trail_3/MainActivity.java
+++ autoRevert_APP/app/src/main/java/com/example/trail_3/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.trail_3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     Button Stop;
     Button Reverse;
 
+    ImageView wifi;
+
+    WifiSocket wifiSocket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,7 +76,48 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Reverse", Toast.LENGTH_SHORT).show();
             }
         });
+
+        wifi=this.findViewById(R.id.imageView);
+        wifi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showIPInputDialog();
+              //  wifiSocket.App2CarWifi(v);
+            }
+        });
 //.
 
     }
+
+
+    private AlertDialog alertDialog;
+    private EditText editText;
+
+
+    private void showIPInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Enter IP Address");
+
+        final EditText input = new EditText(MainActivity.this);
+        builder.setView(input);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String ipAddress = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
 }
