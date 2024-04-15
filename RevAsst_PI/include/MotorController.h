7f2f13fb31e417066d1c63f6dbd939edcbefe32d
Zeeshan Mistry <157168096+ZeeshanMistry@users.noreply.github.com>
2024-04-15 13:22:59 +0100

    motor controller conflict resolved

--- RevAsst_PI/include/MotorController.h
+++ RevAsst_PI/include/MotorController.h
@@ -1,33 +1,51 @@
-#include <gtest/gtest.h>
-#include "MotorController.h"
+#ifndef MOTORCONTROLLER_H
+#define MOTORCONTROLLER_H
 
-class SetMotorDirectionTest : public ::testing::Test {
-protected:
-    MotorController motorController;
-};
+#undef USE_GPIOd 
 
-TEST_F(SetMotorDirectionTest, ForwardDirectionTest) {
-    int input1Pin = 24;
-    int input2Pin = 23;
-    int speedAPin = 12;
-    int direction = 1; // Forward direction
-    int speed = 50;
-    
-    motorController.setMotorDirection(input1Pin, input2Pin, speedAPin, direction, speed);
-    // Add assertions
-}
+#include <stdio.h>
+#include "pigpio.h"
+#include "gpiod.h"
 
-TEST_F(SetMotorDirectionTest, BackwardDirectionTest) {
-    int input1Pin = 24;
-    int input2Pin = 23;
-    int speedAPin = 12;
-    int direction = 0; // Backward direction
-    int speed = 50;
-    
-    motorController.setMotorDirection(input1Pin, input2Pin, speedAPin, direction, speed);
-}
+ // For forward and backward motor control for H bridge
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+#define  input1     24
+#define  input2     23
+#define  speedA     12
+#define  buzzer     26
+
+ // For left and right motor control for H bridge
+#define  input3     25
+#define  input4     5
+#define  speedB     13
+
+class MotorController {
+public:
+
+
+        void forward(int speed);
+        void backward(int speed);
+        void left(int speed);
+        void right(int speed);
+        void forwardRight(int speed);
+        void forwardLeft(int speed);
+        void backwardRight(int speed);
+        void backwardLeft(int speed);
+        void resetMotors();
+        void beep(int time);
+        void terminateGPIO();
+        void motorHandler(char *buffer);
+        void reverseAssistHandler(char *buffer);
+
+
+private:
+   
+
+        int gpioInit(void);
+        void test(int LED_PIN);
+        void testHbridge();
+        void setMotorDirection(int input1Pin, int input2Pin, int speedPin, int direction, int speed);
+        void stopMotor(int speedPin);
+    };
+
+#endif // MOTORCONTROLLER_H
